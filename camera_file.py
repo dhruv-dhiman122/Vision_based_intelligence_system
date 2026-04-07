bcb217fb61c68c68c5e218288cd7403ce5249b13
dhruv dhiman <dhruvdhiman770@gmail.com>
2026-04-07 20:14:02 +0530

    added the code logic for detecting the type of vechile

--- camera_file.py
+++ camera_file.py
@@ -1,1 +1,31 @@
-import opencv as cv
+from ultralytics import YOLO
+import cv2
+
+class VehicleDetector:
+    def __init__(self, model_path="yolov8n.pt"):
+        self.model = YOLO(model_path)
+
+        self.vehicle_classes = [
+            "car", "motorcycle", "bus", "truck", "bicycle"
+        ]
+
+    def detect(self, frame):
+        results = self.model(frame)
+
+        detections = []
+
+        for result in results:
+            for box in result.boxes:
+                cls_id = int(box.cls[0])
+                label = self.model.names[cls_id]
+
+                if label in self.vehicle_classes:
+                    x1,y1,x2,y2 = map(int, box.xyxy[0])
+                    conf = float(box.conf[0])
+
+                    detections.append({
+                        "label": label,
+                        "confidence": conf,
+                        "bbox" : (x1,y1, x2, y2)
+                    })
+        return detections
