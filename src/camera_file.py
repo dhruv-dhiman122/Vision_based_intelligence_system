d20011dfa6c7ae5fb8afefe17d8b19b20edfa532
dhruv dhiman <dhruvdhiman770@gmail.com>
2026-04-12 19:14:09 +0530

    Changed the dict from ml_dict to src

---
+++ src/camera_file.py
@@ -0,0 +1,34 @@
+#This file is for camera system used for detection
+# detection of moving vechiles from different ranges
+
+from ultralytics import YOLO
+import cv2
+
+class VehicleDetector:
+    def __init__(self, model_path="yolov8n.pt"):
+        self.model = YOLO(model_path)
+
+        self.vehicle_classes = [
+            "car", "motorcycle", "bus", "truck", "bicycle"
+        ]
+
+    def detect(self, frame):
+        results = self.model(frame)
+
+        detections = []
+
+        for result in results:
+            for box in result.boxes:
+                cls_id = int(box.cls[0])
+                label = self.model.names[cls_id]
+
+                if label in self.vehicle_classes:
+                    x1,y1,x2,y2 = map(int, box.xyxy[0])
+                    conf = float(box.conf[0])
+
+                    detections.append({
+                        "label": label,
+                        "confidence": conf,
+                        "bbox" : (x1,y1, x2, y2)
+                    })
+        return detections
