1dd3a4c6b737207d700d09988ecd68a6a8787ab7
dhruv dhiman <dhruvdhiman770@gmail.com>
2026-04-12 19:12:51 +0530

    Addded the class for senting notification

--- utils/notifier.py
+++ utils/notifier.py
@@ -2,3 +2,30 @@
 
 import requests
 import time
+
+class Notifier:
+    def __init__(self, token, chat_id):
+        self.token = token
+        self.chat_id = chat_id
+        self.last_sent_time = 0
+        self.cooldown = 60 # seconds
+
+    def sent_alert(self, message):
+        current_time = time.time()
+
+        # Prevents spams
+        if current_time - self.last_sent_time < self.cooldown:
+            return
+
+        url = f"https//api.telegram.org/bot{self.token}/sendMessage"
+
+        data = {
+            "chat_id": self.chat_id,
+            "text": message
+        }
+
+        try:
+            requests.post(url, data = data)
+            self.last_sent_time = current_time
+        except:
+            print("Failed to sent notification")
