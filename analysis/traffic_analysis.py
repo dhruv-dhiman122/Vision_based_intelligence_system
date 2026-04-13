7c65b86bb1af3714718535b1cd536e38365c7c4b
dhruv dhiman <dhruvdhiman770@gmail.com>
2026-04-13 16:38:33 +0530

    Corrected the line 19 error

--- analysis/traffic_analysis.py
+++ analysis/traffic_analysis.py
@@ -1,31 +1,30 @@
 # this file is made for creating charts from the video
 
-import pandas as pd
 import datetime
-import matplotlib.pyplot as plt
 import os
+
+import matplotlib.pyplot as plt
+import pandas as pd
+
 
 class TrafficAnalyzer:
     def __init__(self, output_file="data/traffic_data.csv"):
         self.output_file = output_file
 
-        #Ensure data folder exists
+        # Ensure data folder exists
         os.makedirs(os.path.dirname(self.output_file), exist_ok=True)
 
-        #Load or cerate CSV file
+        # Load or cerate CSV file
         if os.path.exists(self.output_file):
             self.df = pd.read_csv(self.output_file)
         else:
-            self.df = pd.DateFrame(columns=["time","vehicle_count"])
+            self.df = pd.DataFrame(columns=["time", "vehicle_count"])
             self.df.to_csv(self.output_file, index=False)
 
     def log_data(self, vehicle_count):
         current_time = datetime.datetime.now().strftime("%H:%M:%S")
 
-        new_row = {
-            "time" : current_time,
-            "vehicle_count": vehicle_count
-        }
+        new_row = {"time": current_time, "vehicle_count": vehicle_count}
 
         self.df = pd.concat([self.df, pd.DateFrame([new_row])], ignore_index=True)
         self.df.to_csv(self.output_file, index=False)
