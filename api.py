e05ba231f5aaf6f911272ef1ebd022e35a943fb4
dhruv dhiman <dhruvdhiman770@gmail.com>
2026-04-13 17:21:09 +0530

    Made the init module in the code file

--- api.py
+++ api.py
@@ -2,8 +2,20 @@ import cv2
 from flask import Flask, Response, render_template
 
 from analysis.traffic_analysis import TrafficAnalyzer
+from main import analyzer, detector
 from src.camera_file import VehicleDetector
 from src.vehicle_counter import VehicleCounter
 from utils.notifier import Notifier
 
 api = Flask(__name__)
+
+# init module
+detector = VehicleDetector()
+counter = VehicleCounter(line_position=300)
+analyzer = TrafficAnalyzer()
+
+notifier = Notifier("YOUR_TOKEN", "CHAT_ID")
+
+cap = cv2.VideoCapture("video2.mp4")
+
+frame_count = 0
