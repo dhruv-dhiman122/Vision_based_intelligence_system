296daafdde800b573ba71d231abd7edc1a98dd02
dhruv dhiman <dhruvdhiman770@gmail.com>
2026-04-15 19:22:10 +0530

    Added the function for processing message

--- chatbot/learning.py
+++ chatbot/learning.py
@@ -4,3 +4,22 @@
 class TrafficLearningSystem:
     def __init__(self):
         self.learned_data = []
+
+    def process_message(self, message: str, location: str = "Unknown"):
+        message = message.lower()
+
+        traffic_type = None
+
+        if "accident" in message:
+            traffic_type = "accident"
+        elif "jam" in message or "traffic" in message:
+            traffic_type = "heavy"
+        elif "clear" in message:
+            traffic_type = "low"
+
+        if traffic_type:
+            data = {"location": location, "type": traffic_type, "message": message}
+            self.learned_data.append(data)
+            return data
+
+        return None
