b4cec62802c47997eb07dc8b3208e1a4c6716527
dhruv dhiman <dhruvdhiman770@gmail.com>
2026-04-15 15:26:38 +0530

    added the function for adding the reports into the list

--- community/reports.py
+++ community/reports.py
@@ -1,10 +1,25 @@
 # This file is made for driver to help each other on based of the traffic level
 # IDEA TODO -> make the chatbot take more information from this file
 
-form datetime import datetime
+from datetime import datetime
+
 
 class TrafficReportSystem:
-
     def __init__(self, max_reports=100):
-        self.report = []
+        self.reports = []
         self.max_reports = max_reports
+
+    def add_report(self, location: str, message: str):
+        report = {
+            "location": location,
+            "message": message,
+            "timestamp": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+        }
+
+        self.reports.append(report)
+
+        # keep list size controller
+        if len(self.reports) > self.max_reports:
+            self.reports.pop(0)
+
+        return report
