3c7717fe446d375c975c3282a0969ab192ab961f
Dhruv dhiman <dhruvdhiman770@gmail.com>
2026-04-27 18:15:24 +0530

    Added the function of generate in heatmap

--- analysis/heatmap.py
+++ analysis/heatmap.py
@@ -1,3 +1,25 @@
 class HeatmapGenerator:
     def __init__(self, data_store):
         self.data_store = data_store
+
+    def generate(self):
+        df - self.data_store.df
+
+        if df.empty:
+            return {}
+
+        weights = {
+            "low" : 1,
+            "heavy": 3,
+            "accident": 5
+        }
+
+        df = df.copy()
+        df["weight"] = df["event_type"].map(weights).fillna(1)
+
+        heatmap = (
+            df.groupby("location")["weight"]
+            .sum()
+            .to_dict()
+        )
+        return heatmap
